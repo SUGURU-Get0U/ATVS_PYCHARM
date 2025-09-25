ce27034828e21ba5b21af182ce1e4b5ee4a3ecc3
eduardo de oliveira <dudi.cap07@gmail.com>
2025-09-25 18:56:14 -0300

    server.py made the server now need the client

--- revisao/server.py
+++ revisao/server.py
@@ -19,6 +19,34 @@ vamos usar o SOCKET STREAM, no python se referimos a ele como:
 '''
 
 import socket
+import sys
 
-# Vamos criar um objeto para armazenar nosso socket
-server_socket = socket.SOCK_STREAM
+# Hardcode information like PORT and HOST
+
+HOST = "127.0.0.1" # localhost
+PORT = "12345"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # bind the socket to a PORT
+    try:
+        s.bind((HOST,PORT))
+    except:
+        print("Error on binding the socket to a PORT")
+        sys.exit()
+    
+    # now we need to listen for connections
+    s.listen(5)
+    print(f"Listening for connections on PORT: {PORT}")
+    conn, addr = s.accept() # the socket now as conn is waiting for connections
+    with conn as c:
+        print(f"Connection established with {addr}")
+        # now we can receive data from the client
+        while True:
+            data = c.recv(1024) # buffer size is 1024 bytes
+            if not data: # sinalizes a empty packet, meaning the client closed the connection
+                break # so we break the connection loop
+            print(f"Received data: {data.decode()}")
+            c.sendall(data) # echo back the received data
+    print("Client closed the connection")
+    conn.close()
+
